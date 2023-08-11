cccae189c4e1af340a9bba2fd4011654375e0c1c
Idriss <idriss24@hotmail.fr>
2023-08-11 07:15:39 +0200

    little css update

--- assets/app.js
+++ assets/app.js
@@ -12,3 +12,18 @@ import './styles/app.css';
 import './styles/command.css';
 const $ = require('jquery');
 global.$ = global.jQuery = $;
+
+window.addEventListener("scroll", function() {
+    var nav = document.querySelector("nav.navbar");
+    if (window.scrollY > 50) {
+        nav.style.top = window.scrollY + "px";  
+        if (!nav.classList.contains("fixed-position")) {
+            nav.classList.add("fixed-position");
+        }
+    } else {
+        nav.style.top = "0px";
+        if (nav.classList.contains("fixed-position")) {
+            nav.classList.remove("fixed-position");
+        }
+    }
+});
